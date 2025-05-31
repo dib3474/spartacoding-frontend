f29d9fa74bc5ccf667c50cc13076a5f2608d5ce2
dib3474 <dib3474@gmail.com>
2025-05-31 23:15:25 +0900

    ADD: 2번 기능 테스트 작성

--- zz.home-work/src/App.test.tsx
+++ zz.home-work/src/App.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { vi } from 'vitest';
 
 describe('TodoList 통합 테스트', () => {
   it('Todo를 추가하고 완료 체크, 삭제한다', () => {
@@ -36,6 +37,26 @@ describe('TodoList 통합 테스트', () => {
     //when
     fireEvent.change(todoInput, { target: {value: maxText}});
     fireEvent.change(dateInput, { target: {value: '2025-06-02'}});
+    //then
+    const addButton = screen.getByRole('button', { name: 'Add Todo'});
+    expect(addButton).toBeDisabled();
+  });
+
+  it('할 일을 입력할 때 데드라인 날짜가 오늘 날짜 미만이면 입력할 수 없다.', () => {
+    //given
+    const date = new Date(2025, 5, 31);
+
+    vi.useFakeTimers();
+    vi.setSystemTime(date);
+
+    const deadline = new Date(2025, 1, 1)
+
+    render(<App />);
+    const todoInput = screen.getByLabelText('New Todo');
+    const dateInput = screen.getByLabelText('Deadline');
+    //when
+    fireEvent.change(todoInput, { target: {value: 'abc'}});
+    fireEvent.change(dateInput, { target: {value: deadline.toISOString().split('T')[0]}});
     //then
     const addButton = screen.getByRole('button', { name: 'Add Todo'});
     expect(addButton).toBeDisabled();
